3c42ba00f51932663f3fc3cbceeaf5d6bbafb8da
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-08-31 23:58:19 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [jwt, setJwt] = useState("");
+
+  useEffect(() => {
+    const requestBody = {
+      "username":"dozie",
+      "password":"asdfasdf"
+    };
+  
+    fetch("api/auth/login", {
+      headers:{
+        "content-Type": "application/json"
+      },
+      method: "post",
+      body: JSON.stringify(requestBody),
+    })
+    .then((response) => Promise.all([response.json(), response.headers]))
+    .then(([body, headers]) => {
+    
+      setJwt(headers.get("authorization"))
+      console.log(jwt)
+      // console.log(body)
+    });
+  }, []);
+
+useEffect(() => {
+  console.log(`JWT is: ${jwt}`)
+}, [jwt]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, world</h1>
+      <div><h1>Jwt value is </h1><br></br><h2>{jwt}</h2></div>
     </div>
   );
 }
